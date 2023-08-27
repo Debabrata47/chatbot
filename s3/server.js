9c11dbeed554e9fd4f8707b1c0fda859b8bb4f49
Debabrata Mohapatra <debabratamohapatra47@gmail.com>
2023-08-27 16:07:25 +0530

    Files Added

--- s3/server.js
+++ s3/server.js
@@ -1,6 +1,17 @@
-const express = require('express');
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const AWS = require('aws-sdk');
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const express = require('express');
+
+const bucketName = 'audio-test77';
+const jsonKey = 'transcript1.json';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +33,14 @@ async function getObjectURL(key) {
     return url;
 }
 
+
+io.on('connection', socket => {
+    console.log('A client connected');
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+});
+
 app.get('/get-object-url/:key', async (req, res) => {
     try {
         const { key } = req.params;
@@ -32,6 +51,25 @@ app.get('/get-object-url/:key', async (req, res) => {
         res.status(500).json({ error: 'An error occurred' });
     }
 });
+
+app.post('/sns-notification', express.json(), (req, res) => {
+    const message = req.body.Message;
+    console.log('Received SNS notification:', message);
+
+    // Fetch the updated JSON file from S3 and emit to clients
+    s3Client.send(new GetObjectCommand({ Bucket: bucketName, Key: jsonKey }))
+        .then(data => {
+            const jsonData = data.Body.toString('utf-8');
+            io.emit('jsonUpdate', jsonData);
+        })
+        .catch(err => {
+            console.error('Error fetching JSON file:', err);
+        });
+
+    res.sendStatus(200);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
